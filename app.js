5bbd6c905c26e75673ae338591bf3f4e1c350fed
Luke Turner <luke@lukedturner.com>
2019-07-09 17:33:55 +1000

    Setting up meetup auth with docs

--- app.js
+++ app.js
@@ -3,10 +3,35 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//Additional packages
+
+//Passport
+
+const passport = require("./config/passport");
+app.use(passport.initialize());
+
 //Root page 
 app.get('/', (req,res) => {
     res.send('Testing!');
 });
+
+//Meetup API test
+
+app.get('/meetup',
+  passport.authenticate('meetup', { session: false }),
+  function(req, res) {
+    res.json(req.user);
+  });
+
+passport.use(new MeetupOAuth2Strategy({
+    clientID: MEETUP_KEY,
+    clientSecret: MEETUP_SECRET,
+    callbackURL: "http://127.0.0.1:3000/auth/meetup/callback",
+    autoGenerateUsername: true
+  }, function(accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+  }));
+
 
 //Application-level middleware
 
