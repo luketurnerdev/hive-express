2de040f21a127db42047df5e5df87fd6114ae615
Luke Turner <luke@lukedturner.com>
2019-07-12 09:02:17 +1000

    Added some comments to users controller

---
+++ controllers/users_controller.js
@@ -0,0 +1,7 @@
+function create() {
+    //Create a new user when they sign up
+    //Most of this information will be pulled from the Meetup API
+
+    //We may need to insert checks to determine if the user already exists in the DB or not.
+}
+
