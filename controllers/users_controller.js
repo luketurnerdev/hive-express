cc917e791cfc00807a27473c52cc3404d2b6ff35
Luke Turner <luke@lukedturner.com>
2019-07-12 13:28:42 +1000

    created route /auth/register, and made create() method in controller which successfully creates a new user when done via postman

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
+//Import the user model
+const User = require("./../database/models/user_model");
+
 function index(req, res) {
     res.send("Users index");
   }
@@ -7,6 +10,45 @@ function create() {
     //Most of this information will be pulled from the Meetup API
 
     //We may need to insert checks to determine if the user already exists in the DB or not.
+}
+
+async function create(req, res) {
+    let {
+      meetup_uid,
+      email,
+      password,
+      firstName,
+      lastName,
+      city,
+      avatar,
+      admin,
+      confirmed,
+      access_token,
+      refresh_token,
+      created_at,
+      updated_at
+    } = req.body;
+
+    let user = await User.create({
+      meetup_uid,
+      email,
+      password,
+      firstName,
+      lastName,
+      city,
+      avatar,
+      admin,
+      confirmed,
+      access_token,
+      refresh_token,
+      created_at,
+      updated_at
+      }).catch(err =>
+        res.status(500).send(err)
+      );
+      
+      res.send(req.body);
+
 }
 
 // async function create(req, res) {
