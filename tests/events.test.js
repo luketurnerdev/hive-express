5dd1245b3bb879c62e13499f09b2663d1a1d7cc9
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-07-22 22:36:12 +1000

    (wip) continuing to setup tests

--- tests/events.test.js
+++ tests/events.test.js
@@ -1,6 +1,7 @@
-const eventsController = require("./../controllers/events_controller");
-const EventModel = require("./../database/models/event_model");
+const request = require("supertest");
+const app = require("./../app");
 
-test('eventsController.index returns an array', () => {
-  expect(eventsController.index).toContain();
+test('eventsController.index returns 200', () => {
+  return request(app).get("/events").expect(200);
+  //expect(eventsController.index).toContain();
 });
