418102e92e5382f09af6744efef9b4c42c6c866e
Jim Farrugia <the,jim.farrugia@gmail.com>
2019-07-24 13:55:01 +1000

    added test for GET to '/events'

--- tests/events.test.js
+++ tests/events.test.js
@@ -1,9 +1,30 @@
 const eventsController = require("./../controllers/events_controller");
 const EventModel = require("./../database/models/event_model");
+const httpMocks = require('node-mocks-http');
 const request = require("supertest");
 const app = require("./../app");
 
-test('/events returns 200', () => {
-  //jest.setTimeout(30000);
-  return request(app).get("/events").expect(200);
+describe('Test events routes', () => {
+  test('GET - "/events" response is defined', () => {
+    const request = httpMocks.createRequest({
+      method: 'GET',
+      url: '/events'
+    });
+    const response = httpMocks.createResponse();
+
+    eventsController.index(request, response, (err) => {
+      expect(err).toBeFalsy();
+    });
+
+    let data = response._getData();
+
+    expect(data).toBeDefined();
+  });
+
+  
 });
+
+// test('/events returns 200', () => {
+//   //jest.setTimeout(30000);
+//   return request(app).get("/events").expect(200);
+// });
