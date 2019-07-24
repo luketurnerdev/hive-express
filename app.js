4a416d14f434f5aca5a5747db63ebd97fa916f74
Luke Turner <luke@lukedturner.com>
2019-07-24 14:43:25 +1000

    deleted conflict error

--- app.js
+++ app.js
@@ -39,37 +39,5 @@ app.use(require("./middleware/error_handler_middleware"));
 
 //Enable use of process.env calls
 require("dotenv").config();
-<<<<<<< HEAD
-client_id = process.env.CLIENT_ID;
-client_secret = process.env.CLIENT_SECRET;
-redirect_uri = process.env.REDIRECT_URI;
-//Additional packages
-const axios = require('axios');
-const queryString = require('query-string');
-
-//Passport
-// const passport = require("./config/passport");
-// app.use(passport.initialize());
-
-//Meetup authentication
-const meetupAuth = require('./controllers/auth_controller').meetupAuth;
-// const tester = require('./controllers/auth_controller').tester;
-const meetupService = require("./services/MeetupService");
-
-//Root page 
-app.get('/', (req,res) => {
-    let access_token, refresh_token;
-    //Has there been a token defined? If so, assign a variable
-
-    if (meetupService.getItem('current-user')){
-        access_token = meetupService.getItem('current-user').access_token;
-        refresh_token = meetupService.getItem('current-user').refresh_token;
-    }
-    res.send(`Testing. Access token: ${access_token}, refresh: ${refresh_token}`);
-    console.log(meetupService);
-
-});
-=======
->>>>>>> 29fd11c8818f45fb3a67cc7729afbffa24bd0f62
 
 module.exports = app;
